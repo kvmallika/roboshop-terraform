fd32e312b8e85494829805be68262c03f9379ff9
Mallika <kmallika.malli@gmail.com>
2023-05-23 16:10:29 +0530

    printing the output

--- servers.tf
+++ servers.tf
@@ -1,12 +1,12 @@
 data "aws_ami" "centos" {
-  owners = "973714476881"
+  owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 
 }
 
 output "ami_id"{
-  value=data.ams_ami.centos.image_id
+  value = data.aws_ami.centos.image_id
 }
 
   resource "aws_instance" "frontend" {
