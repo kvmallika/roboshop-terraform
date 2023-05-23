6b26907872c4563054cacb69ac8d35810296de9b
Mallika <kmallika.malli@gmail.com>
2023-05-23 16:23:05 +0530

    creating dns record

--- servers.tf
+++ servers.tf
@@ -23,7 +23,7 @@ resource "aws_route53_record" "frontend" {
   name    = "frontend-dev.vemdevops.online"
   type    = "A"
   ttl     = 30
-  records = aws_instance.frontend.private_ip
+  records = [aws_instance.frontend.private_ip]
 }
 
 
