c1bab54dd83d3aae8fd83fd064880733a8607843
Mallika <kmallika.malli@gmail.com>
2023-05-23 16:06:56 +0530

    printing the output

--- servers.tf
+++ servers.tf
@@ -1,4 +1,15 @@
-resource "aws_instance" "frontend" {
+data "aws_ami" "centos" {
+  owners = "973714476881"
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+
+}
+
+output "ami_id"{
+  value=data.ams_ami.centos.image_id
+}
+
+  resource "aws_instance" "frontend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
 instance_type = "t3.micro"
 
@@ -6,6 +17,8 @@ tags = {
   Name = "frontend"
 }
 }
+
+
 resource "aws_instance" "mongodb" {
 ami           = "ami-0b5a2b5b8f2be4ec2"
 instance_type = "t3.micro"
