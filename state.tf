f568d9bc3cb01f32716e4f1d510cca7b377c411c
Mallika <kmallika.malli@gmail.com>
2023-06-06 13:02:21 +0530

    state file

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "roboshop-vemdevops"
-    key    = "roboshop/dev/terraform.tfstate"
+    key    = "roboshop/terraform.tfstate"
     region = "us-east-1"
   }
 }
