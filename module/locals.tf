b0d04defa286e48ce8506205caed848290933b8b
Mallika <kmallika.malli@gmail.com>
2023-07-20 11:58:34 +0530

    added code in local file

--- module/locals.tf
+++ module/locals.tf
@@ -2,7 +2,7 @@ locals {
   name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   app_commands = [
     "sudo labauto ansible" ,
-    "ansible-pull -i localhost, -u https://github.com/kvmallika/roboshop-ansible roboshop.yml -e env=${env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -u https://github.com/kvmallika/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name} "
     ]
   db_commands = [
     "rm -rf roboshop-shell",
