69a6d075cb4ab69fb2e719316e890644727e85de
Mallika <kmallika.malli@gmail.com>
2023-10-04 23:05:13 +0530

    changes in ssm parameters and tf-module-db

--- main.tf
+++ main.tf
@@ -38,13 +38,14 @@ module "docdb" {
 
   env = var.env
   kms_arn = var.kms_arn
-  engine_version = var.engine_version
   tags = local.tags
   vpc_id = local.vpc_id
 
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main" ,null ), "subnets" , null), each.value["subnet_name"],null),"subnet_ids",null)
 
+  engine_version = each.value[engine_version]
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main" ,null ), "subnets" , null), each.value["allow_db_cidr"],null),"subnet_cidrs",null)
 }
+
 
 
