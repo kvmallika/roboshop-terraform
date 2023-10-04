f1c4b7d27c91bba627fb75c414b2a40a4b7d0b44
Mallika <kmallika.malli@gmail.com>
2023-10-04 23:05:38 +0530

    changes in ssm parameters and tf-module-db

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "docdb" {
 
   subnets = lookup(lookup(lookup(lookup(module.vpc, "main" ,null ), "subnets" , null), each.value["subnet_name"],null),"subnet_ids",null)
 
-  engine_version = each.value[engine_version]
+  engine_version = each.value["engine_version"]
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main" ,null ), "subnets" , null), each.value["allow_db_cidr"],null),"subnet_cidrs",null)
 }
 
