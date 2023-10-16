453b6150c59e2faaa25c1ab5bbe009edbb049e99
Mallika <kmallika.malli@gmail.com>
2023-10-16 12:56:27 +0530

    IAM profile created to fetch the parameters

--- main.tf
+++ main.tf
@@ -109,6 +109,7 @@ module "app" {
   tags               = local.tags
   domain_name        = var.domain_name
   domain_id          = var.domain_id
+  kms_arn            = var.kms_arn
   dns_name           = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
 
   instance_type    = each.value["instance_type"]
