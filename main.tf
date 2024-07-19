11726c0cb7959dbb31667a38b6e29013a1f008b7
Mallika <kmallika.malli@gmail.com>
2024-07-19 14:01:06 +0530

    included all the databases(dockdb, rds, rabbitMQ, elasticache) in main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "vpc" {
   default_vpc_rtid = var.default_vpc_rtid
 }
 
-/*module "docdb" {
+module "docdb" {
   source = "git::https://github.com/kvmallika/tf-module-docdb.git"
   for_each = var.docdb
 
@@ -84,7 +84,7 @@ module "rabbitmq" {
 
 }
 
-module "alb" {
+/*module "alb" {
   source = "git::https://github.com/kvmallika/tf-module-alb.git"
   for_each = var.alb
 
